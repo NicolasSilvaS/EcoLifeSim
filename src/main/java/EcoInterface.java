0b7a341c39f5ba511dab142e90828a7d359b347d
gustavoWuerges <gugu.wuerges@gmail.com>
2025-09-04 21:55:21 -0300

    classe interface

---
+++ src/main/java/EcoInterface.java
@@ -0,0 +1,26 @@
+package main.java;
+
+public interface EcoInterface {
+    
+    //Alimentar
+    void comer();
+    void beber();
+
+    //locomoção/lazer
+    void dormir();
+    void mover();
+    void atacar();
+    void cacar();
+    void avistar();
+    void nadar();
+    void acasalar();
+
+    //barulhos
+    void fazerBarulho();
+    void quebrar();
+    
+    
+
+    
+    
+}
